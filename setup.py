bbf08069a80dd181c12d4505c2acf4e8933a3998
valmur prado <pradovalmur@gmail.com>
2021-01-25 21:21:03 -0300

    Commit Inicial - conserta Bug s3.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 
 setuptools.setup(
-    name="aws_cdk_exemplo",
+    name="s3_stack",
     version="0.0.1",
 
     description="An empty CDK Python app",
@@ -15,8 +15,8 @@ setuptools.setup(
 
     author="author",
 
-    package_dir={"": "aws_cdk_exemplo"},
-    packages=setuptools.find_packages(where="aws_cdk_exemplo"),
+    package_dir={"": "s3_stack"},
+    packages=setuptools.find_packages(where="s3_stack"),
 
     install_requires=[
         "aws-cdk.core==1.86.0",
